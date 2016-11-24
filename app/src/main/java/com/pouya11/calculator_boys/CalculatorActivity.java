56be99d8e80fef2119c4a6f055c91d683dcbc4a1
Mohammad <m.nourinik@irantalent.com>
2016-11-24 14:57:19 +0330

    implement operation buttons

--- app/src/main/java/com/pouya11/calculator_boys/CalculatorActivity.java
+++ app/src/main/java/com/pouya11/calculator_boys/CalculatorActivity.java
@@ -5,11 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
 
     Button[] btnNumbers = new Button[10];
+    Button btnDot;
+    Button btnClear;
+    Button btnClearEverything;
+    Button btnSum;
+    Button btnSub;
+    Button btnDivide;
+    Button btnMultiply;
+    Button btnResult;
+    Button btnExit;
     EditText txtNumber;
+
+    double dMemory = 0;
+    int iState = CalculatorActivity.STATE_NA;
+
+    protected static final int STATE_NA = 0;
+    protected static final int STATE_SUM = 0;
+    protected static final int STATE_SUB = 0;
+    protected static final int STATE_MULTIPLY = 0;
+    protected static final int STATE_DIVIDE = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +46,75 @@ public class CalculatorActivity extends AppCompatActivity {
         btnNumbers[7] = (Button) findViewById(R.id.btn7);
         btnNumbers[8] = (Button) findViewById(R.id.btn8);
         btnNumbers[9] = (Button) findViewById(R.id.btn9);
+        btnDot = (Button) findViewById(R.id.btn_dot);
+        btnClear = (Button) findViewById(R.id.btn_clear);
+        btnClearEverything = (Button) findViewById(R.id.btn_clear_everything);
+        btnSum = (Button) findViewById(R.id.btn_sum);
+        btnSub = (Button) findViewById(R.id.btn_sub);
+        btnDivide = (Button) findViewById(R.id.btn_divide);
+        btnMultiply = (Button) findViewById(R.id.btn_multiply);
+        btnResult = (Button) findViewById(R.id.btn_result);
+        btnExit = (Button) findViewById(R.id.btn_exit);
         txtNumber = (EditText) findViewById(R.id.txtNumber);
 
         for(Button b: btnNumbers) {
             b.setOnClickListener(new BtnNumbersClicked());
         }
+        btnClear.setOnClickListener(new BtnClearClicked());
+        btnSum.setOnClickListener(new BtnOperationClicked());
+        btnSub.setOnClickListener(new BtnOperationClicked());
+        btnMultiply.setOnClickListener(new BtnOperationClicked());
+        btnDivide.setOnClickListener(new BtnOperationClicked());
     }
 
-    class BtnNumbersClicked implements View.OnClickListener {
+    protected void clear(){
+        txtNumber.setText("");
+    }
+
+    protected class BtnNumbersClicked implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
             CalculatorActivity.this.txtNumber.append(((Button) v).getText());
+        }
+    }
+
+    protected class BtnClearClicked implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            CalculatorActivity.this.clear();
+        }
+    }
+
+    protected class BtnOperationClicked implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_sum:
+                    CalculatorActivity.this.iState = CalculatorActivity.STATE_SUM;
+                    break;
+                case R.id.btn_sub:
+                    CalculatorActivity.this.iState = CalculatorActivity.STATE_SUB;
+                    break;
+                case R.id.btn_multiply:
+                    CalculatorActivity.this.iState = CalculatorActivity.STATE_MULTIPLY;
+                    break;
+                case R.id.btn_divide:
+                    CalculatorActivity.this.iState = CalculatorActivity.STATE_DIVIDE;
+                    break;
+                default:
+                    CalculatorActivity.this.iState = CalculatorActivity.STATE_NA;
+            }
+            try {
+                CalculatorActivity.this.dMemory = Double.parseDouble(txtNumber.getText().toString());
+            }catch (Exception e){
+                Toast.makeText(CalculatorActivity.this,
+                        R.string.something_wrong,
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
         }
     }
 }
